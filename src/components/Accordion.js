f857a7806858eb64943e6b86ed661f5f50fb15a6
Victor <victor.du.77@hotmail.fr>
2021-05-14 11:04:47 +0200

    Expanding the Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -9,12 +9,15 @@ const Accordion = ({ items }) => {
     }
 
     const renderedItem = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : ''
+
         return <React.Fragment key={item.title}>
-            <div className="title active" onClick={() => onTitleClick(index)}>
+            <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
         </React.Fragment>
