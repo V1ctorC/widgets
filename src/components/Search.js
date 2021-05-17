4563ac0e60690b3fd65871b84967b6536a82cf11
Victor <victor.du.77@hotmail.fr>
2021-05-17 19:22:10 +0200

    Async Code in useEffect + Executing the Request from useEffect + Default Search Terms

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Search = () => {
 
-    const [term, setTerm] = useState('')
+    const [term, setTerm] = useState('programming')
+    const [result, setResult] = useState([])
+
+    console.log(result)
 
     useEffect(() => {
-        console.log('I only run once')
+
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+
+            setResult(data.query.search)
+        }
+
+        search()
+        
+        
     }, [term])
 
     return (
