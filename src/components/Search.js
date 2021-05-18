553cfbc62113abb26a4a893d433619651c13f858
Victor <victor.du.77@hotmail.fr>
2021-05-18 19:24:22 +0200

    Fixing a Warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,18 @@ import axios from 'axios';
 const Search = () => {
 
     const [term, setTerm] = useState('programming')
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([])
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000)
+
+        return (() => {
+            clearTimeout(timerId)
+        })
+    }, [term])
 
     useEffect(() => {
 
@@ -15,28 +26,16 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             })
 
             setResults(data.query.search)
         }
 
-        if (term && !results.length) {
-            search()
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search()
-                }
-            }, 500)
-            
-            return (() => {
-                clearTimeout(timeoutId)
-            })
-        }
+        search()
 
-    }, [term])
+    }, [debouncedTerm])
 
     const rederedResults = results.map((result) => {
         return (
